7a7a990c58e0a41c4059e83ca65c9a3129152f1f
smjtd <55692060+smjtd@users.noreply.github.com>
2021-05-12 12:54:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,6 +13,6 @@ resource "null_resource" "sleep" {
   }
 
   provisioner "local-exec" {
-    command = "sleep ${var.sleepy_time}"
+    command = "echo $command = "echo $aa" \n sleep ${var.sleepy_time}"
   }
 }
